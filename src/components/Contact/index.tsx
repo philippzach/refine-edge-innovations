e16e1bf91e8a27e00eebff7eaa06d9d05e207695
Philipp Zach <philippzach@Philipps-Mini.Home>
2024-10-01 12:03:06 +0100

    added metal choiing

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -76,7 +76,10 @@ const Contact = () => {
                       >
                         Art von Metall
                       </label>
-                      <select className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none">
+                      <select
+                        name="metal"
+                        className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
+                      >
                         <option value="">Wähle eine Option</option>
                         <option value="Stahl">Stahl</option>
                         <option value="Edelstahl">Edelstahl</option>
